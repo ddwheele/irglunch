e5ac798e0738425f870b0197b2e7ab96fd612180
DW Wheeler <dawn.wheeler@gmail.com>
2019-06-11 19:47:28 -0700

    implement next month page

--- catalog/urls.py
+++ catalog/urls.py
@@ -4,10 +4,12 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('archive', views.archive, name='archive'),
+    path('next_month', views.next_month, name='next-month'),
     path('<int:year>/<int:month>/<int:day>', views.single_lunch_listing, name = 'single-lunch-listing'),
     path('<int:year>/<int:month>/<int:day>/add_guest', views.add_guest, name='add-guest'),
     path('<uuid:pk>/change_host', views.change_host, name='change-host'),
-    path('calculate_month_of_assignments', views.calculate_month_of_assignments),    
+    path('calculate_current_month_assignments', views.calculate_current_month_assignments),    
+    path('calculate_next_month_assignments', views.calculate_next_month_assignments),    
     path('reset_hosted_dates', views.reset_hosted_dates)
 ]
     
--- catalog/views.py
+++ catalog/views.py
@@ -16,41 +16,24 @@ IMMUNITY_PERIOD = 31 # after you host, you won't be asked to host again for this
 # then the value is the date of the first Tuesday
 tues_translator = {1:2, 2:1, 3:7, 4:6, 5:5, 6:4, 7:3}
 
-def assign_host(lunchdate):
-    immune_date = lunchdate - datetime.timedelta(days=IMMUNITY_PERIOD)
-    people = Person.objects.filter(active = True, last_hosted__lte=immune_date)
-    denominator = 0 # total chances of anybody being picked to host
-    for peep in people:
-        denominator += peep.num_guest_actions - HOST_COEFFICIENT * peep.num_host_actions
- 
-    selected = random.randint(0,denominator)
-    chances = 0
-    for peep in people:
-        chances += peep.num_guest_actions - HOST_COEFFICIENT * peep.num_host_actions
-        if selected <= chances:    
-            ha = HostAction(date=lunchdate, host=peep)
-            ha.save()
-            peep.num_host_actions = peep.num_host_actions + 1
-            peep.last_hosted = lunchdate
-            peep.save()
-            return 
-
-    #return people
-
-def reset_hosted_dates(request):
-    people = Person.objects.all()
-    for peep in people:
-        peep.last_hosted = datetime.datetime(year=2000, month=5, day =5)
-        peep.save()
-    context = {}
-    context['peeps'] = people
-    return render(request, 'catalog/debug.html', context=context)
 
 @login_required
-def calculate_month_of_assignments(request):
-    """"View function for home page"""
+def calculate_current_month_assignments(request):
     today = datetime.date.today()
-    the_first = today.replace(day=1)
+    calculate_month_of_assignments(today)
+    return HttpResponseRedirect('/')
+
+@login_required
+def calculate_next_month_assignments(request):
+    today = datetime.date.today()
+    day_next_month = get_next_month(today)
+    calculate_month_of_assignments(day_next_month)
+    return HttpResponseRedirect('next_month')
+
+# takes in a day, creates host assignments for each Tuesday in that month
+def calculate_month_of_assignments(a_day):
+    """"View function for home page"""
+    the_first = a_day.replace(day=1)
     weekday = the_first.isoweekday()
     first_tuesday = the_first.replace( day=tues_translator[weekday] )
 
@@ -59,9 +42,38 @@ def calculate_month_of_assignments(request):
     for i in range(5):
         next_tues = first_tuesday + i*week
         if next_tues.month == first_tuesday.month:
-            assign_host(next_tues)
+            # make sure there isn't already a host assigned to that day
+            host_action = HostAction.objects.filter(date=next_tues)
+            if host_action.exists():
+                continue
+            else:
+                assign_host(next_tues)
     return HttpResponseRedirect('/')
 
+def assign_host(lunchdate):
+    immune_date = lunchdate - datetime.timedelta(days=IMMUNITY_PERIOD)
+    people = Person.objects.filter(active = True, last_hosted__lte=immune_date)
+    denominator = 0 # total chances of anybody being picked to host
+    for peep in people:
+        denominator += peep.num_guest_actions - HOST_COEFFICIENT * peep.num_host_actions
+
+    if denominator > 0: 
+        selected = random.randint(0,denominator)
+        chances = 0
+        for peep in people:
+            chances += peep.num_guest_actions - HOST_COEFFICIENT * peep.num_host_actions
+            if selected <= chances:    
+                ha = HostAction(date=lunchdate, host=peep)
+                ha.save()
+                peep.num_host_actions = peep.num_host_actions + 1
+                peep.last_hosted = lunchdate
+                peep.save()
+                return 
+    else:
+        # If we weren't able to assign anybody, give to "unassigned"
+        nobody = Person.objects.filter(name='unassigned')
+        ha = HostAction(date=lunchdate, host=nobody[0])
+        ha.save()
 
 @login_required
 def single_lunch_listing(request, year, month, day):
@@ -89,16 +101,30 @@ def index(request):
     today = datetime.date.today()
     host_action_list = HostAction.objects.filter(date__year = today.year,
                                             date__month = today.month).order_by('date')
-    context['host'] = host_action_list
+    context['host_actions'] = host_action_list
 
     return render(request, 'index.html', context=context)
 
+@login_required
 def archive(request):
     context = {}
     today = datetime.date.today()
     host_action_list = HostAction.objects.filter(date__lte=today).order_by('-date')
     context['host_actions'] = host_action_list
     return render(request, 'archive.html', context=context)
+
+
+@login_required
+def next_month(request):
+    context = {}
+    today = datetime.date.today()
+    day_next_month = get_next_month(today)
+    host_action_list = HostAction.objects.filter(date__year = day_next_month.year,
+                                                 date__month = day_next_month.month)
+    context['host_actions'] = host_action_list
+
+    return render(request, 'next_month.html', context=context)
+  
 
 @login_required
 def add_guest(request, year, month, day):
@@ -162,7 +188,26 @@ def change_host(request, pk):
 
     return render(request, 'catalog/change_host.html', context)
 
+# used for debugging
+def reset_hosted_dates(request):
+    people = Person.objects.all()
+    for peep in people:
+        peep.last_hosted = datetime.datetime(year=2000, month=5, day =5)
+        peep.save()
+    context = {}
+    context['peeps'] = people
+    return render(request, 'catalog/debug.html', context=context)
 
+# given a date, return a date in the next month
+def get_next_month(today):
+    next_month = today.month + 1
+    if next_month == 13:
+        next_year = today.year + 1
+        day_next_month = today.replace(month=1, year=next_year)
+    else:
+        day_next_month = today.replace(month=next_month)
+    return day_next_month
+ 
 
 
 
