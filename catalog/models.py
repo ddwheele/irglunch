9b5fadcb0da6319fc49acbef4e55c8a9fa78fdec
DW Wheeler <dawn.wheeler@gmail.com>
2019-09-16 22:43:56 -0700

    Prompt good citizenship

--- catalog/models.py
+++ catalog/models.py
@@ -8,7 +8,7 @@ class Person(models.Model):
     """Represents a person who hosts or eats IRG lunch"""
     name = models.CharField(max_length=50, help_text="Enter person's name or nickname")
     active = models.BooleanField(default=True, help_text='Is this person a possible IRG lunch host?')
-    num_guest_actions = models.IntegerField(default=0, help_text='Times person has attended IRG lunch')
+    num_guest_actions = models.IntegerField(default=1, help_text='Times person has attended IRG lunch')
     num_host_actions = models.IntegerField(default=0, help_text='Times person has hosted IRG lunch')
     last_hosted = models.DateField(default=dt.datetime(year=dt.MINYEAR,month=1,day=1),
                                    help_text='Date of last time person hosted IRG lunch')
